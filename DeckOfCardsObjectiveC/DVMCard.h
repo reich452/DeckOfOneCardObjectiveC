0d9a79aefd11a0b944579435b99c6f39c5541647
Nick Reichard <reich452@regis.edu>
2018-06-26 10:54:22 -0600

    Updated UI and added DVMCardObject

---
+++ DeckOfCardsObjectiveC/DVMCard.h
@@ -0,0 +1,18 @@
+//
+//  DVMCard.h
+//  DeckOfCardsObjectiveC
+//
+//  Created by Nick Reichard on 6/26/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DVMCard : NSObject
+
+@property (nonatomic, copy, readonly) NSString *suit;
+@property (nonatomic, copy, readonly) NSString *image;
+
+- (instancetype)initWithSuit:(NSString *)suit image:(NSString *)image;
+
+@end
---
+++ DeckOfCardsObjectiveC/DVMCard.m
@@ -0,0 +1,18 @@
+//
+//  DVMCard.m
+//  DeckOfCardsObjectiveC
+//
+//  Created by Nick Reichard on 6/26/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+#import "DVMCard.h"
+
+@implementation DVMCard
+
+- (instancetype)initWithSuit:(NSString *)suit image:(NSString *)image
+{
+   
+}
+
+@end
